dd5d04c933812ad7404a75999d0382fd6c199628
vimalraj11 <84240545+vimalraj11@users.noreply.github.com>
2026-08-24 23:31:47 +0530

    Add files via upload

--- setupRemoteMongo.sh
+++ setupRemoteMongo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-MONGO_URI="mongodb://ac-6mezhzw-shard-00-00.cgkjjuz.mongodb.net:27017,ac-6mezhzw-shard-00-01.cgkjjuz.mongodb.net:27017,ac-6mezhzw-shard-00-02.cgkjjuz.mongodb.net:27017/qkart?replicaSet=atlas-by58hg-shard-0&authSource=admin"
+MONGODB_URL=mongodb+srv://siviraj725_db_user:QkartMongo2026@qkart-node.cgkjjuz.mongodb.net/qkart?retryWrites=true
 
 USERNAME="siviraj725_db_user"
-PASSWORD="4YUezCVVAWEo5zpm"
+PASSWORD="QkartMongo2026"
 
 mongoimport --uri "$MONGO_URI" \
   --ssl \
